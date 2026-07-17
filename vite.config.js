bb7cc825bf7a15b3e89fb48223c524166c3f817c
Diana Nguyen <diana.nguyen@epita.fr>
2026-07-17 10:22:45 +0200

    please update :( (cache edit)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: '/EUROBAZAR_web/',
+  base: command === 'serve' ? '/' : '/EUROBAZAR_web/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
